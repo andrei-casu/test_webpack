f7634cd45eb5a6b4fa4d613765a60d0ed6595e67
casuneanuandrei <casuneanu.andrei97@gmail.com>
2016-04-11 13:56:26 +0300

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-<<<<<<< HEAD
-    // target: 'node', // in order to ignore built-in modules like path, fs, etc. 
-    // externals: [nodeExternals()],//ignore from bundle external libraries
-    // target: 'web',
-=======
     target: 'web',
->>>>>>> 23d6b0a89b201d86c4468f5abafaa99be2dc5c55
     entry: {
         app: "./app",
         vendor: ["jquery", "bootstrap"]
@@ -21,7 +15,9 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
+            // { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.css$/,
+              loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader") },
             { test: /\.scss$/,
               loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader") },
             { test: /\.less$/,
@@ -37,13 +33,8 @@ module.exports = {
     },
     
     plugins: [
-<<<<<<< HEAD
-        new ExtractTextPlugin("app.css", {allChunks: false}),
-        // new webpack.optimize.CommonsChunkPlugin("bootstrap", "vendor.css"),
-=======
         new ExtractTextPlugin("app.css", {allChunks: true}),
         //new webpack.optimize.CommonsChunkPlugin("bootstrap", "vendor.css"),
->>>>>>> 23d6b0a89b201d86c4468f5abafaa99be2dc5c55
         new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.js"),
         new webpack.ProvidePlugin({
             $: "jquery",
